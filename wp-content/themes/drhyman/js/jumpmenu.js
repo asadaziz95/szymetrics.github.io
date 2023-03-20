10d518c4d1c0b3a5843b3161f2f4684c3855f0a9
asadaziz <asadazizjs@gmail.com>
2023-03-20 22:33:05 +0500

    Added

---
+++ wp-content/themes/drhyman/js/jumpmenu.js
@@ -0,0 +1,1 @@
+let mainNavLinks=document.querySelectorAll("nav.jumpto_menu ul li a");window.addEventListener("scroll",event=>{let fromTop=window.scrollY+1+100;mainNavLinks.forEach(link=>{let section=document.querySelector(link.hash);if(section.offsetTop<=fromTop&&section.offsetTop+section.offsetHeight>fromTop){link.classList.add("current");}else{link.classList.remove("current");}});});
